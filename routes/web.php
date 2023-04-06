1d923cf9b1105a95373c48c6158bbfa043c3e60c
FatkhiAlauddin <cupetongspeed12@gmail.com>
2023-04-07 04:51:43 +0700

    migration, database seeder, factory

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
